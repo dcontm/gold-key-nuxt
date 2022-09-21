1064dee9cd386c4bc0c55bc018829bcb0ed05104
dcontm <dcontm@gmail.com>
2022-09-21 22:02:46 +0300

    navbar 0.010

---
+++ components/DekstopNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="h-10 my-2 bg-blue-400 justify-center">
+    <ul class="inline-flex items-center">
+      <li>Главная</li>
+      <li>Детский сад</li>
+      <li>Кружки</li>
+      <li>Контакты</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-
+  <div class="max-w-7xl px-4">
+    <header-block />
+  </div>
 </template>
 
 <script>
+import HeaderBlock from '@/components/HeaderBlock.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    HeaderBlock
+  }
+
 }
 </script>
