be6859174430c63275c43589537e347d94dbc401
dcontm <dcontm@gmail.com>
2022-09-22 23:25:56 +0300

    add banner

---
+++ components/Navigation.vue
@@ -0,0 +1,123 @@
+<template>
+  <nav class=" w-full p-2 bg-transparent h-20 md:px-4">
+    <div class="flex items-center justify-between">
+
+      <!-- Header logo -->
+      <div>
+        <img src="@/static/logo-gold-key.jpg" class="object-cover h-20">
+      </div>
+
+      <!-- Mobile toggle -->
+      <div class="md:hidden">
+        <button @click="drawer">
+          <svg 
+            class="h-8 w-8 fill-current text-red-500"
+            fill="none" stroke-linecap="round" 
+            stroke-linejoin="round" stroke-width="2" 
+            viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+
+      <!-- Navbar -->
+      <div class="hidden md:block">
+        <ul class="flex space-x-4 md:text-base lg:text-lg font-sans font-medium text-teal-500 hover:text-teal-700">
+          <li><a href="hover:border-teal-500" class="active text-teal-700 border-b-2 border-red-500 pb-1">Главная</a></li>
+          <li><a href="#" class="hover:border-b-2 hover:border-yellow-500 hover:pb-1">Сад</a></li>
+          <li><a href="#" class="hover:border-b-2 hover:border-yellow-500 hover:pb-1">Кружки</a></li>
+          <li><a href="#" class="hover:border-b-2 hover:border-yellow-500 hover:pb-1">Новости</a></li>
+          <li><a href="#" class="hover:border-b-2 hover:border-yellow-500 hover:pb-1">Контакты</a></li>
+          <li><a href="#" class="cta bg-teal-500 hover:bg-teal-700 hover:border-2 hover:border-yellow-500 px-3 py-2 rounded-full text-white font-semibold">Войти</a></li>
+        </ul>
+      </div>
+
+      <!-- Dark Background Transition -->
+      <transition
+        enter-class="opacity-0"
+        enter-active-class="ease-out transition-medium"
+        enter-to-class="opacity-100"
+        leave-class="opacity-100"
+        leave-active-class="ease-out transition-medium"
+        leave-to-class="opacity-0"
+      >
+        <div @keydown.esc="isOpen = false" v-show="isOpen" class="z-10 fixed inset-0 transition-opacity">
+            <div @click="isOpen = false" class="absolute inset-0 bg-black opacity-50" tabindex="0"></div>
+        </div>
+      </transition>
+
+      <!-- Drawer Menu -->
+      <aside class="p-5 transform top-0 left-0 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30" :class="isOpen ? 'translate-x-0' : '-translate-x-full'">
+        
+        <div class="close">
+          <button class="absolute top-0 right-0 mt-4 mr-4" @click=" isOpen = false">
+            <svg 
+              class="w-6 h-6 text-red-500"
+              fill="none" stroke-linecap="round" 
+              stroke-linejoin="round" stroke-width="2"
+              viewBox="0 0 24 24" stroke="currentColor">
+              <path d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+          </button>
+        </div>
+
+        <span @click="isOpen = false" class="flex w-full items-center p-4 border-b">
+        </span>
+
+        <ul class="divide-y font-sans  text-teal-500 hover:text-teal-700">
+          <li><a href="#" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Главная</a></li>
+          <li><a href="#" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Сад</a></li>
+          <li><a href="#" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Кружки</a></li>
+          <li><a href="#" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Новости</a></li>
+          <li><a href="#" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Контакты</a></li>
+          <li><a href="#" @click="isOpen = false" class="my-8 w-full text-center font-semibold cta inline-block bg-teal-500 hover:bg-teal-700 hover:border-2  hover:border-yellow-500 px-3 py-2 rounded-full text-white">Войти</a></li>
+        </ul>
+
+        <div class="follow">
+          <p class="font-sans text-teal-500 text-sm">Мы в соцсетях:</p>
+          <div class="social flex space-x-5 mt-4 ">
+            <a href="https://vk.com/goldkey21">
+              <img src="@/static/vk-logo.svg" class="w-10 h-10">
+            </a>
+            <a href="https://www.instagram.com/detskijsadik21.ru">
+              <img src="@/static/instagram-logo.svg" class="w-10 h-10">
+            </a>
+          </div>
+        </div>
+
+      </aside>
+
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    drawer() {
+      this.isOpen = !this.isOpen;
+    }
+  },
+  watch: {
+    isOpen: {
+      immediate: true,
+      handler(isOpen) {
+        if (process.client) {
+          if (isOpen) document.body.style.setProperty("overflow", "hidden");
+          else document.body.style.removeProperty("overflow");
+        }
+      }
+    }
+  },
+  mounted() {
+    document.addEventListener("keydown", e => {
+      if (e.keyCode == 27 && this.isOpen) this.isOpen = false;
+    });
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="max-w-7xl px-4">
-    <header-block />
+  <div class="max-w-7xl">
+    <banner />
+    <navigation />
+    <div class="flex-wrap md:flex pt-8 justify-center">
+      <div class="md:w-1/2">
+        <p class="text-3xl text-red-600 text-bold font-serif">Частный детский сад НЮР г.Чебоксары</p>
+      </div>
+      <img src="@/static/main-img.png" class="object-cover h-[500px] md:h-[300px]">
+    </div>
   </div>
 </template>
 
 <script>
-import HeaderBlock from '@/components/HeaderBlock.vue'
+import Banner from "@/components/Banner.vue"
+import Navigation from '~/components/Navigation.vue'
 export default {
   name: 'IndexPage',
   components: {
-    HeaderBlock
+    Navigation,
+    Banner
   }
 
 }
