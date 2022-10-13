4f4bb489784f19bb6b382fd34bc83f7fc499ccd3
dcontm <dcontm@gmail.com>
2022-10-13 23:55:52 +0300

    upgrade news/

--- pages/news.vue
+++ pages/news.vue
@@ -2,14 +2,23 @@
   <div>
     <banner />
     <navigation />
-    <div class="text-center text-xl md:text-2xl text-teal-700 my-10">
-        <p>Данный раздел находиться в разработке. Пожайлуста, приходите позже...</p>
-        <div class="flex justify-center items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-48 h-58 md:w-80 md:h-80 text-red-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
-            </svg>
-        </div>
+    <div class="text-center font-serif text-3xl text-teal-700 my-10">
+      <span>Наши новости ...</span>
     </div>
+    <div class="mx-4 md:mx-10 lg:mx-12 my-10 grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="" v-for="post in posts" :key="post.id">
+        <div :id="get_post_id(post.id)"></div>
+        <script type="text/javascript">
+          (function() {
+            console.log(window.innerWidth)
+            VK.Widgets.Post(`vk_post_-43822083_{{post.id}}`, -43822083, {{post.id}}, `{{post.hash}}`);
+          }());
+        </script>
+      </div>
+    </div>
+    <a class="flex justify-center" href="https://vk.com/goldkey21" target="_blank">
+      <button class="text-center text-white font-serif text-bold text-2xl py-3 px-5 bg-teal-600 rounded-2xl mb-5">Ещё больше новостей в нашей группе VK</button>
+    </a>
     <footer-bar />
   </div>
 </template>
@@ -24,10 +33,29 @@ export default {
     Navigation,
     FooterBar
   },
+  head: {
+    script: [
+      {
+        src: "https://vk.com/js/api/openapi.js?169",
+      }  
+    ]
+
+  },
   data() {
     return {
+      posts: [],
+      group_id:"43822083"
     }
   },
+  async fetch() {
+    const response = await this.$axios.$get('/news')
+    this.posts = response.items
+  },
+  methods: {
+    get_post_id (id) {
+      return "vk_post_-43822083_"+id
+    }
+  }
 
 
 }
