893f7552b1b3087a9a28fe7efc69a255a2b4fcc6
dcontm <dcontm@gmail.com>
2022-10-16 22:54:59 +0300

    navigation fix error

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -84,9 +84,11 @@
           <li><NuxtLink to="/news" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Новости</NuxtLink></li>
           <li><NuxtLink to="/kontakty" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Контакты</NuxtLink></li>
           <li v-if="!this.$auth.loggedIn"><NuxtLink to="/login" @click="isOpen = false" class="my-8 w-full text-center font-semibold cta inline-block bg-teal-500 hover:bg-teal-700 hover:border-2  hover:border-yellow-500 px-3 py-2 rounded-full text-white">Войти</NuxtLink></li>
-          <li v-if="this.$auth.loggedIn"><NuxtLink to="/dashboard" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Кабинет</NuxtLink></li>
-          <li v-if="this.$auth.user.is_superuser"><NuxtLink to="/dashboard" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Админ</NuxtLink></li>
-          <li v-if="this.$auth.loggedIn"><NuxtLink to="/" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Выйти</NuxtLink></li>
+          <div v-if="this.$auth.loggedIn" class="divide-y">
+            <li><NuxtLink to="/dashboard" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Кабинет</NuxtLink></li>
+            <li v-if="this.$auth.user.is_superuser"><NuxtLink to="/dashboard" @click="isOpen = false" class="my-4 inline-block hover:border-b-2 hover:border-yellow-500 hover:pb-1">Админ</NuxtLink></li>
+            <li @click="logout"><NuxtLink to="#" @click="isOpen = false" class="my-4 inline-block border-2 border-blue-400 px-3 rounded-full hover:border-yellow-500 text-teal-900">Выйти</NuxtLink></li>
+          </div>
         </ul>
 
         <div class="follow">
